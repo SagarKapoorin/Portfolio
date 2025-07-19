55101a8a57ea8b17a23ed1da8910ac7a689536bb
sagarhanabitech <sagar@hanabitech.com>
2025-07-19 11:44:29 +0530

    updated responsive of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
 
       <div className="min-h-screen w-full transition-opacity duration-1000 ease-in-out opacity-1 animate-fadeIn flex flex-col items-center">
-        <h1 className="mt-8 text-outline-double max-sm:text-6xl mid-sm:mid2-sm:text-10xl sm:text-9xl font-doner leading-[0.9] font-bold text-white tracking-tight text-center">
+        <h1 className="mt-20 text-outline-double max-sm:text-6xl mid-sm:mid2-sm:text-10xl sm:text-9xl font-doner leading-[0.9] font-bold text-white tracking-tight text-center">
           SAGAR <br /> KAPOOR
         </h1>
         <div className="w-screen">
