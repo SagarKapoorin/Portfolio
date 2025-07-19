ab2dca1f0a975c6cb15fa083097931c1de888c34
sagarhanabitech <sagar@hanabitech.com>
2025-07-19 14:39:12 +0530

    code cleanup

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,8 +4,4 @@ const redis = createClient({ url: process.env.REDIS_URL });
 redis.on('error', (err) => console.error('Redis Client Error', err));
 redis.connect().catch((err) => console.error('Redis connection error', err));
 
-const subscriber = redis.duplicate();
-subscriber.on('error', (err) => console.error('Redis Subscriber Error', err));
-subscriber.connect().catch((err) => console.error('Redis subscriber error', err));
-
-export { redis, subscriber };
+export { redis };
